617b9b08406e99ae06ef632c5a2d053839587e88
Jon Kuci <jon.kuci@student.uni-pr.edu>
2023-12-24 01:05:55 +0100

    Javascript ne rentify dhe finalizim

--- UEB23_24_Gr12/RentifyYourHouse.js
+++ UEB23_24_Gr12/RentifyYourHouse.js
@@ -1,7 +1,7 @@
-var canvas = document.getElementById("katrori");
-  var ctx = canvas.getContext("2d");
+var canva = document.getElementById("katrori");
+  var ctx = canva.getContext("2d");
 
-  // Set line properties
+
   ctx.lineWidth = 2;
   ctx.strokeStyle =  "#002B5B"; // Blue color
   
@@ -18,3 +18,34 @@ var canvas = document.getElementById("katrori");
   ctx.moveTo(50, 100);
   ctx.lineTo(350, 100);
   ctx.stroke();
+
+
+  var Lease_Agreement = document.getElementById('Lease_Agreement');
+  var RLease_Agreement = document.getElementById('RLease_Agreement');
+  var Repairs_and_maintenance = document.getElementById('Repairs_and_maintenance');
+  var Insurancet = document.getElementById('Insurancet');
+  var Legal_Responsibilities = document.getElementById('Legale'); 
+
+  var Iagreetoall = document.getElementById('Iagreetoall');
+
+  var buttonRentify = document.getElementById('buttonRentify');
+
+  function handleCheckboxChange() {
+    if (Lease_Agreement.checked && RLease_Agreement.checked &&
+        Repairs_and_maintenance.checked && Insurancet.checked && Legal_Responsibilities.checked) {
+      console.log("All checkboxes are checked");
+      Iagreetoall.disabled = false; 
+      buttonRentify.disabled = false;
+    } else {
+      console.log("Not all checkboxes are checked");
+      Iagreetoall.disabled = true;
+      buttonRentify.disabled = true;
+    }
+  }
+
+  Lease_Agreement.addEventListener('change', handleCheckboxChange);
+  RLease_Agreement.addEventListener('change', handleCheckboxChange);
+  Repairs_and_maintenance.addEventListener('change', handleCheckboxChange);
+  Insurancet.addEventListener('change', handleCheckboxChange);
+  Legal_Responsibilities.addEventListener('change', handleCheckboxChange);
+
